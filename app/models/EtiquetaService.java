015e96257927566e970edeccb1a458c8a45c068e
Héctor Compañ <hectordorito5@gmail.com>
2015-12-09 10:55:46 +0100

    Clase DAO y Servicios de Etiqueta
    Las etiquetas tienen un usuario asociado
    Service llama a las funciones del DAO
    Modificar y Eliminar tareas.

--- app/models/EtiquetaService.java
+++ app/models/EtiquetaService.java
@@ -10,4 +10,26 @@ public class EtiquetaService {
     public static Etiqueta grabaEtiqueta(Etiqueta etiqueta) {
         return EtiquetaDAO.create(etiqueta);
     }
+
+    public static List<Etiqueta> findAllEtiquetasUsuario(Integer usuarioId) {
+        Usuario usuario = UsuarioDAO.find(usuarioId);
+        return usuario.etiquetas;
+    }
+
+    public static Etiqueta findEtiqueta(Integer id) {
+        return EtiquetaDAO.find(id);
+    }
+
+    public static boolean deleteEtiqueta(Integer id) {
+        try {
+            EtiquetaDAO.delete(id);
+        } catch(EntityNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static Etiqueta modificaEtiqueta(Etiqueta etiqueta) {
+        return EtiquetaDAO.update(etiqueta);
+    }
 }
