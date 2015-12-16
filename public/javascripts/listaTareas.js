c5afe98a65497fa48104dc249d4f1b95f0d40693
EncarnaAmoros <encaramorosb@gmail.com>
2015-12-16 18:06:23 +0100

    Refactor e icono para mostrar anotacion
    Todo el javascript listaTareas en un .js a parte e incluido en el html
    Icono mostrar anotacion y cerrar anotacion del acordeon (va cambiando)

---
+++ public/javascripts/listaTareas.js
@@ -0,0 +1,98 @@
+/* CRUD CON LAS TAREAS DE UN USUARIO */
+
+/* Para eliminar una tarea */
+
+function del(urlBorrar) {
+  $.ajax({
+      url: urlBorrar,
+      type: 'DELETE',
+      success: function(results) {
+          //refresh the page
+          location.reload();
+      }
+  });
+}
+
+/* Para modificar el estado de una tarea */
+
+function modificarEstado(urlModificar, id, descripcion, estado, idusuario, anotacion, prioridad) {
+  if(estado == 'pendiente') nuevo_estado = 'realizada';
+  else nuevo_estado = 'pendiente';
+  $.ajax({
+   url: urlModificar,
+   data : { id : id, descripcion : descripcion, estado : nuevo_estado, id_usuario : idusuario, anotacion : anotacion, prioridad : prioridad },
+   type: 'POST',
+   success: function(results) {
+     location.reload();
+   }
+  });
+}
+
+/* Para modificar una tarea */
+
+function modificarTareaAnotacion(urlModificar, id, descripcion, estado, idusuario, anotacionA, prioridad) {
+  var anotacion;
+  if(anotacionA=='borrar') anotacion = "";
+  else anotacion = document.getElementById("textoanotacion"+id).value;
+
+  $.ajax({
+   url: urlModificar,
+   data : { id : id, descripcion : descripcion, estado : estado, id_usuario : idusuario, anotacion: anotacion, prioridad : prioridad },
+   type: 'POST',
+   success: function(results) {
+     //Sin recargar la página y cerramos acordeon
+     anotacion.value = anotacion;
+     cerrarAnotacion(id);
+     mostrarSuccessAnotacion();
+   }
+  });
+}
+
+/* Para modificar la prioridad de una tarea */
+
+function guardarTareaPrioridad(urlModificar, id, descripcion, estado, idusuario, anotacion, prioridad) {
+  $.ajax({
+   url: urlModificar,
+   data : { id : id, descripcion : descripcion, estado : estado, id_usuario : idusuario, anotacion: anotacion, prioridad : prioridad },
+   type: 'POST',
+   success: function(results) {
+     location.reload();
+   }
+  });
+}
+
+
+/* OPERACIONES CON ACORDEÓN PARA ANOTACIÓN TAREA */
+
+/* Para cerrar/abrir/efectos del acordeón de la anotación de cada tarea */
+
+function abrirAnotacion(id) {
+  $(".accordion-content"+id).slideDown(0);
+  $("#iconoabriranotacion").addClass("iconoinvisible");
+  $("#iconocerraranotacion").removeClass("iconoinvisible");
+}
+
+function cerrarAnotacion(id) {
+  $(".accordion-content"+id).slideUp(0);
+  $("#iconocerraranotacion").addClass("iconoinvisible");
+  $("#iconoabriranotacion").removeClass("iconoinvisible");
+}
+
+function mostrarAcordeon(id) {
+    var contenido=$(".accordion-content"+id);
+     if(contenido.css("display")=="none"){ //Abrimos acordeon
+       abrirAnotacion(id);
+     }
+     else{ //Cerramos acordeon
+       cerrarAnotacion(id);
+    }
+}
+
+function mostrarSuccessAnotacion() {
+  $.smkAlert({
+    text: 'Anotación modificada correctamente.',
+    type: 'success',
+    position:'top-center',
+    time: 2
+  });
+}
