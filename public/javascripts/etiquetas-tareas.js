99a587fdab22ab3135d4227787946792cb69682b
Héctor Compañ <hectordorito5@gmail.com>
2015-12-20 17:50:59 +0100

    Dragula funcionando en formCrearTarea

--- public/javascripts/etiquetas-tareas.js
+++ public/javascripts/etiquetas-tareas.js
@@ -21,10 +21,10 @@ function actualizar_etiquetas(idusuario) {
                 if($.inArray(key,almacen_tags_tarea)<0) //si esa tag no esta añadida al almacen, la añadimos a nuestra lista de tags
                 {
                     var boton = "<div class='btn-group'>";
-                    boton+= "<a href='#' data-toggle='tooltip' onclick='add_tag(" + key + ")' title='Click para añadir' onmouseenter='pon_verde(event)' onmouseleave='pon_azul(event)' class='btn btn-xs btn-info'><span class='glyphicon glyphicon-tag'></span>" + value + "</a>";
+                    boton+= "<a href='#' data-toggle='tooltip' class='btn btn-xs btn-info'><span class='glyphicon glyphicon-tag'></span>" + value + "</a>";
                     boton+="<a href='#' data-toggle='tooltip' onclick='borrar_tag_bd(" + key + ")' title='Click para eliminar completamente' class='btn btn-xs btn-warning'><span class='glyphicon glyphicon-trash'></span></a>";
                     boton+="</div>";
-                    var plantilla = '<li id=usuariotag' + key + '>' + boton + '</li>';
+                    var plantilla = '<li id=' + key + '>' + boton + '</li>';
                     tags_usuario.innerHTML+=plantilla;
                 }
                 plantilla = '<option value="' + key + '">' + value + '</option>'
@@ -46,9 +46,6 @@ function mostrar_tags(tareaId, idusuario) {
         url: '/tareas/' + tareaId + '/etiquetas',
         type: 'GET',
         success: function(results) {
-
-            var listaTags= document.getElementById('right-events');
-            listaTags.innerHTML="";
             //añadir las etiquetas que ya tenia
             $.each(results, function(key, value) {
                 var boton = "<div data-toggle='tooltip' title='Click para eliminar' class='btn-xs btn-info'><span class='glyphicon glyphicon-tag'></span>" + value + "</a><a href='#' class='btn-xs btn-info' onclick='remove_tag(" + key + ")'><span class='glyphicon glyphicon-remove'></span></a></div>";
@@ -62,44 +59,6 @@ function mostrar_tags(tareaId, idusuario) {
       actualizar_etiquetas(idusuario);
 }
 
-function pon_verde(event)
-{
-    $(event.target).addClass("btn-success");
-    $(event.target).removeClass("btn-info");
-    $(event.target.children).addClass("glyphicon-plus-sign");
-    $(event.target.children).removeClass("glyphicon-tag");
-}
-
-function pon_azul(event)
-{
-    $(event.target).removeClass( "btn-success"); //si esta en verde, cambia a azul
-    $(event.target).removeClass( "btn-warning"); //si esta en amarillo, cambia a azul
-    $(event.target).addClass( "btn-info");
-
-
-    $(event.target.children).removeClass("glyphicon-plus-sign"); //lo mismo
-    $(event.target.children).removeClass("glyphicon-minus-sign");
-    $(event.target.children).addClass("glyphicon-tag");
-}
-
-function add_tag(id) {
-
-    var text = $("#usuariotag" + id)[0].innerText;
-
-    var boton = "<div data-toggle='tooltip' title='Click para eliminar' class='btn-xs btn-info'><span class='glyphicon glyphicon-tag'></span>" + text + "</a><a href='#' class='btn-xs btn-info' onclick='remove_tag(" + id + ")'><span class='glyphicon glyphicon-remove'></span></a></div>";
-    var plantilla = '<li id=' + id + '>' + boton + '</li>';
-    document.getElementById('tags-selecc').innerHTML+=plantilla;
-    actualizar_almacen_tags();
-    $("#usuariotag" + id).remove(); //lo eliminamos de nuestras etiquetas
-
-}
-
-function remove_tag(id) {
-     $("#" + id).remove();
-     actualizar_almacen_tags();
-     var idusuario = $('[name="id_usuario"]').val();
-     actualizar_etiquetas(idusuario);
-}
 
 function editar_etiqueta(idusuario) {
     var data = {};
@@ -141,13 +100,15 @@ function replace_tag() {
     var almacen_tags_tarea = document.getElementById('almacen_tags').value;
     almacen_tags_tarea = almacen_tags_tarea.split(";");
 
-    console.log(almacen_tags_tarea);
-    console.log("LA TAG ES " + tag_id);
     if($.inArray(tag_id,almacen_tags_tarea)>=0) //si esta en el almacen de tags, ahi hay problema
     {
-        console.log("SALSEO");
+
         var text = $('#modif-tag').val();
-        var boton = "<div data-toggle='tooltip' title='Click para eliminar' class='btn-xs btn-info'><span class='glyphicon glyphicon-tag'></span>" + text + "</a><a href='#' class='btn-xs btn-info' onclick='remove_tag(" + tag_id + ")'><span class='glyphicon glyphicon-remove'></span></a></div>";
+
+        var boton = "<div class='btn-group'>";
+        boton+= "<a href='#' data-toggle='tooltip' class='btn btn-xs btn-info'><span class='glyphicon glyphicon-tag'></span>" + text + "</a>";
+        boton+="<a href='#' data-toggle='tooltip' onclick='borrar_tag_bd(" + tag_id + ")' title='Click para eliminar completamente' class='btn btn-xs btn-warning'><span class='glyphicon glyphicon-trash'></span></a>";
+        boton+="</div>";
         $('#' + tag_id).html(boton);
         actualizar_almacen_tags();
 
@@ -162,7 +123,7 @@ function replace_tag() {
 
 function actualizar_almacen_tags() {
     var almacen = document.getElementById("almacen_tags");
-    var listItems = $("#tags-selecc li");
+    var listItems = $("#left-events li");
 
     almacen.value=""; //limpiamos el almacen
     listItems.each(function(idx, li)
@@ -204,8 +165,8 @@ function borrar_tag_bd(id) {
           url: '/etiquetas/' + id,
           type: 'DELETE',
           success: function(results) {
+            $("#" + id).remove();
             actualizar_etiquetas(idusuario); //actualizamos listas
-            remove_tag(id); //borramos de los tags seleccionados, ya que no puede tener seleccionada una tag no existente
             actualizar_almacen_tags(); //actualizamos el almacen oculto de tags para enviar en el post
 
             },
@@ -254,14 +215,12 @@ dragula([$('left-events'), $('right-events')])
 
     //Si se ha soltado arriba se añade la etiqueta a la tarea
     if(padre.id=="left-events") {
-      alert("Estamos arriba");
-
+      actualizar_almacen_tags();
     }
 
     //Si se ha soltado abajo se elimina la etiqueta de la tarea
     if (padre.id=="right-events") {
-      alert("Estamos abajo");
-      
+      actualizar_almacen_tags();
     }
 
   })
